d2897cac5d054d3879090276f44304521b286cad
hamed <abdulhamed341@gmail.com>
2022-07-25 16:49:37 +0530

    file folder

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace imagespractice.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
